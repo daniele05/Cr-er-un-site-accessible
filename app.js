7d3beef511496e3f50da489b655a86b4929ef475
daniele05 <konda300@yahoo.fr>
2021-09-15 23:08:05 +0200

    insertion file json, insertion des premiers tags et de quelques elements css

--- app.js
+++ app.js
@@ -1,47 +1,22 @@
-// // Demande en amont au serveur
-// OPTIONS /data HTTP/1.1
-// Origin: https://example.com
-// Access-Control-Request-Method: DELETE 
 
 //recuperation des donnees Json
 
-fetch("https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeData.json/request")
-.then(function(res){
-    if(res.ok) {
-        return res.json();  
-    }
-})
-.then(function(value) {
-    console.log(value);
-})
-.catch(function(error) {
- // erreur survenue
-});
-
-
-// var xhttp = new XMLHttpRequest();
-// xhttp.open("GET", 'https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeData.json')
-// xhttp.onreadystatechange = function(){
-//     if(xhttp.readyState === 4) {
-//         console.log(xhttp.responseText);
-//     } 
-// };
-
-
-// xhttp.send();
+var myInit = { method: 'GET',
+           headers: {
+               'Content-Type': 'application/json'
+           },
+          mode:'cors',
+          cache: 'default' };        
 
 
 
+let myRequest = new Request ("./app.json", myInit);
 
-
-// // // Correction du cors 
-// // router.get('/' , function (req, res, next) {
-// //     axios.get('https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeData.json')
-// //    then(Response => {
-// //        res.send(Response.data.result);
-// //    }) . catch(error => {
-// //        res.send(error.message);
-// //    })
-// //    }); 
-
+fetch(myRequest)
+  .then(function(resp){
+      return resp.json();
+  })
+  .then (function(app){
+   console.log(app.photographers);
+  });
     
