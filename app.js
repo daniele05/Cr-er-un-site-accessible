26a80b19b2d20599ad588aabf4b00c92e0d2195f
daniele05 <konda300@yahoo.fr>
2021-09-21 13:40:57 +0200

    modification initiée telechargement donnees, travail avec grid et autres

--- app.js
+++ app.js
@@ -1,25 +1,53 @@
+// Autorisation de toutes les sources
+//Puis dans le second header, les headers htpp qui sont acceptes 
 
-//recuperation des donnees Json
+var app; 
+app.use(function (request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-let myInit = {
-  method: 'GET',
-  headers: {
-    Accept: 'application/json',
-    'Content-type': 'application/json',
-  },
-  mode: 'cors',
-  cache: 'default',
-};      
+// Dès qu'une requête de type options est envoyé à une url de l'api
+// Le serveur répond qu'il accepte les méthodes GET, PUT, POST, DELETE et OPTIONS
+app.options('/api/*', function (request, response, next) {
+  response.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  response.send();
+});
 
 
+//Déclaration des var header et section dans js 
 
-let myRequest = new Request ("./data.json", myInit);
+var header = document.querySelector('header'); 
+var section = document.querySelector('section');
 
-fetch("./data.json")
-  .then(function(resp){
-      return resp.json();
-  })
-  .then (function(data){
-   console.log(data.photographers);
-  });
-    
+// Requete faite au serveur pour la recuperation des donnees json
+
+var requestURL = 'https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeData.json'; 
+var request = new XMLHttpRequest(); 
+request.open('GET' , requestURL); 
+request.responsiveType = 'json'; 
+request.send(); 
+
+request.onload = function(){
+    var photographersFreelance = request.response;
+    populateHeader(photographersFreelance); 
+    showPhotographersList(photographersFreelance);
+}
+
+// Declaration de la function et manipulation des elements du DOM 
+function populateHeader(jsonObj) {
+  var myH1 = document.createElement('h1');
+  myH1.textContent = jsonObj['Les photographes']; 
+  header.appendChild(myH1)
+}
+
+// Creation de fiches photographes
+
+function showPhotographersList (jsonObj) {
+  var photographers = jsonObj['members'];
+  for(var i = 0; i < photographers.lenght; i++) {
+    link.push('<a href="#">'+ i + '</a>')
+
+  }
+}
